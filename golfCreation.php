6d4d5d9cf9fc0bc765d2ae53dd1c8ecba8551f7e
Maxime-Missichini <Thmeynder@gmail.com>
2021-04-11 11:02:19 +0200

    -Working on golf creation via map, XML creation on the way with few upgrades

--- golfCreation.php
+++ golfCreation.php
@@ -1,3 +1,21 @@
+<script>
+  // Function to create the cookie
+  function createCookie(name, value, minutes) {
+    var expires;
+
+    if (minutes) {
+      var date = new Date();
+      date.setTime(date.getTime() + (minutes * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+    } else {
+      expires = "";
+    }
+
+    document.cookie = escape(name) + "=" +
+      escape(value) + expires + "; path=/";
+  }
+</script>
+
 <?php
 
 include('server.php');
@@ -17,6 +35,8 @@ if(isset($_GET['destroy'])){
   ?>
 <script>
   sessionStorage.clear();
+  createCookie('grnname',"",-1);
+  createCookie('grntrou',"",-1);
 </script>
 <?php
 }
@@ -25,24 +45,6 @@ if(isset($_GET['destroy'])){
 
 <!DOCTYPE html>
 <html lang="">
-
-<script>
-  // Function to create the cookie
-  function createCookie(name, value, minutes) {
-    var expires;
-
-    if (minutes) {
-      var date = new Date();
-      date.setTime(date.getTime() + (minutes * 60 * 1000));
-      expires = "; expires=" + date.toGMTString();
-    } else {
-      expires = "";
-    }
-
-    document.cookie = escape(name) + "=" +
-      escape(value) + expires + "; path=/";
-  }
-</script>
 
 <head>
   <title>GreenSa - Golf Creation</title>
@@ -58,6 +60,9 @@ if(isset($_GET['destroy'])){
   <button class="logout"><a href="index.php?logout='1'">Log out</a></button>
   <button class="destroySession"><a href="golfCreation.php?destroy='1'">Destroy session</a></button>
   <button class="backGolf"><a href="golf.php">Back to my golfs</a></button>
+  <form method="post" action="golfCreation.php">
+    <button class="createXML" type="submit" name="submit_xml">Create XML</button>
+  </form>
 </div>
 
 <div class="form-popup" id="nameForm">
@@ -79,7 +84,6 @@ if(isset($_GET['destroy'])){
 
   if(sessionStorage.getItem("clicked") !== "1") {
     createCookie("grntrou", 1, "10");
-    console.log("eheh");
     document.getElementById("nameForm").style.display = "block";
     document.getElementById("main_container").style.display = "none";
   }
@@ -141,7 +145,7 @@ if(isset($_GET['destroy'])){
     })
     </script>
 <?php
-  //SUBMIT TROU
+  //SUBMIT GOLF NAME
   if(isset($_POST['submit_name'])){
     ?>
 
@@ -155,6 +159,8 @@ if(isset($_GET['destroy'])){
 
     <?php
   }
+
+  //SUBMIT GOLF HOLE
   if (isset($_POST['submit_trou'])){
 
       $latitude = 0;
@@ -168,12 +174,9 @@ if(isset($_GET['destroy'])){
       if(mysqli_num_rows($results) === 0){
         $nbTrou = 9;
         $query = "INSERT INTO golf (idGolf,nbTrou) VALUES ('$golfname','$nbTrou')";
-        echo "ah";
         mysqli_query($db,$query);
       }
-
       $trou = $_COOKIE['grntrou'];
-
 
       if (isset($_COOKIE['grnlat']) && isset($_COOKIE['grnlng'])) {
         $latitude = $_COOKIE['grnlat'];
@@ -184,6 +187,40 @@ if(isset($_GET['destroy'])){
       $query = "INSERT INTO trougolf (idGolf,trou,par,lat,lng) VALUES ('$golfname','$trou','$par','$latitude','$longitude')";
       mysqli_query($db, $query) or print "erreur";
       setcookie('grntrou',$trou+1);
+  }
+
+  if (isset($_POST['submit_xml'])){
+
+    $db = mysqli_connect('localhost', 'root', '', 'demo') or die('Could not connect to the database');
+    $xml = new SimpleXMLElement('<xml/>');
+    $nbTrou = $_COOKIE['grntrou'];
+    $nom = $_COOKIE['grnname'];
+    $golfCourse = $xml->addChild('GolfCourse');
+    $name = $golfCourse->addChild('Name',$nom);
+    $nbTrous = $golfCourse->addChild('NbTrous',$nbTrou);
+    $nomgolf = $golfCourse->addChild('NomGolf',$name);
+    $coordinates = $golfCourse->addChild('Coordinates');
+
+    for ($i=1;$i<=$nbTrou;$i++){
+      $trou = $coordinates->addChild('Trou');
+
+      $parquery = mysqli_query($db, "SELECT par FROM trougolf WHERE idGolf='$nom' AND trou='$i'");
+      $pars = mysqli_fetch_row($parquery);
+
+      $latsquery = mysqli_query($db, "SELECT lat FROM trougolf WHERE idGolf='$nom' AND trou='$i'");
+      $lats = mysqli_fetch_row($latsquery);
+
+      $lngquery = mysqli_query($db, "SELECT lng FROM trougolf WHERE idGolf='$nom' AND trou='$i'");
+      $lngs = mysqli_fetch_row($lngquery);
+      $test = strval($pars[0]);
+      echo "<script> console.log('$test'); </script>";
+      $par = $trou->addChild('par',strval($pars[0]));
+      $lat = $trou->addChild('lat',strval($lats[0]));
+      $lng = $trou->addChild('lng',strval($lngs[0]));
+    }
+    $filename = 'golf.xml';
+    $xml->saveXML($filename);
+    echo "<label><a href='golf.xml'>Here is your file</a></label>";
   }
 ?>
 
